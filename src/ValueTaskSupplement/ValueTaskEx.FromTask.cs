484d74d4918f4d09d74fe64ff3ba732b6f04791a
Takaaki Suzuki <xin9le@live.jp>
2019-08-27 15:26:17 +0900

    Add Task -> ValueTask conversion APIs.

---
+++ src/ValueTaskSupplement/ValueTaskEx.FromTask.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+
+
+namespace ValueTaskSupplement
+{
+    public static partial class ValueTaskEx
+    {
+        public static ValueTask FromTask(Task result)
+            => new ValueTask(result);
+
+
+        public static ValueTask<T> FromTask<T>(Task<T> result)
+            => new ValueTask<T>(result);
+
+
+        public static ValueTask AsValueTask(this Task result)
+            => new ValueTask(result);
+
+
+        public static ValueTask<T> AsValueTask<T>(this Task<T> result)
+            => new ValueTask<T>(result);
+    }
+}
